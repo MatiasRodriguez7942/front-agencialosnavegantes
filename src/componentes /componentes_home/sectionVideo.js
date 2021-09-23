f1b39009c05296f5436834fcc6a07cf9feb09b40
Matias97788 <m.rodriguez@agencialosnavegantes.cl>
2021-09-22 21:28:59 -0300

    prueba 2

--- src/componentes /componentes_home/sectionVideo.js
+++ src/componentes /componentes_home/sectionVideo.js
@@ -1,7 +1,8 @@
 import React from "react";
 import UseHome from "../../hooks/useHome";
 import "../../../node_modules/react-modal-video/scss/modal-video.scss";
-import ModalVideo from "react-modal-video";
+// import ModalVideo from "react-modal-video";
+import ModalVideo from "react-modal-video"
 function SectionVideo() {
   const response = UseHome();
   const Datos = response.allStrapiHome.nodes[0].home_seccion7;
