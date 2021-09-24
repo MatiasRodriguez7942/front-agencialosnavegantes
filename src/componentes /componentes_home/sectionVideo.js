57535f2fbe52ff1defa77bdac9e9047f366c753c
Matias97788 <m.rodriguez@agencialosnavegantes.cl>
2021-09-24 16:08:01 -0300

    prueba

--- src/componentes /componentes_home/sectionVideo.js
+++ src/componentes /componentes_home/sectionVideo.js
@@ -1,29 +1,50 @@
 import React from "react";
 import UseHome from "../../hooks/useHome";
-import "../../../node_modules/react-modal-video/scss/modal-video.scss";
-import ModalVideo from "react-modal-video"
+import { Modal, Nav, Container, Row, Col, Button } from "react-bootstrap";
+
 function SectionVideo() {
   const response = UseHome();
   const Datos = response.allStrapiHome.nodes[0].home_seccion7;
-  const [isOpen, setOpen] = React.useState(false);
 
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div
       style={{
         alignItems: "center",
         justifyContent: "center",
         textAlign: "center",
-        paddingBottom:"40px",
-        paddingTop:"40px" ,
+        paddingBottom: "40px",
+        paddingTop: "40px",
       }}
     >
-      <ModalVideo
-        channel="youtube"
-        autoplay
-        isOpen={isOpen}
-        videoId="gB3F83WhmzY"
-        onClose={() => setOpen(false)}
-      />
+      <Modal show={show} onHide={handleClose}>
+       
+        <Modal.Body>
+         <Container>
+         <Row>
+            <Col sm={2}></Col>
+            <Col sm={8}> <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/gB3F83WhmzY"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe></Col>
+            <Col sm={2}> <Button onClick={handleClose}> CERRAR</Button></Col>
+
+
+          </Row>
+         </Container>
+         
+         
+        </Modal.Body>
+     
+      </Modal>
       <div
         style={{
           backgroundImage: `url("https://agencia-navegantes.s3.amazonaws.com/imgvideo2_1_f7cace4ae8.jpg")`,
@@ -32,7 +53,9 @@ function SectionVideo() {
           backgroundPosition: "center",
         }}
       >
-     <button className="btn-primary" onClick={()=> setOpen(true)}>VIEW DEMO</button>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
       </div>
     </div>
   );
